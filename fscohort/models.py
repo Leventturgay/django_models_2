69de68cf1f49c795c51a9558673b552ac3d0f9b4
Turgay Levent <turgayleventt@gmail.com>
2023-01-03 12:51:39 +0300

    models3

--- fscohort/models.py
+++ fscohort/models.py
@@ -4,10 +4,21 @@ from django.db import models
 
 
 class Student(models.Model):
-    first_name = models.CharField(max_length=30, blank=True)
+    first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=40)
-    number = models.PositiveSmallIntegerField(blank=True)
+    number = models.PositiveSmallIntegerField(blank=True, null=True)
+    about = models.TextField(blank=True)
+    email = models.EmailField(blank=True)
+    is_active = models.BooleanField(default=True)
+    avatar = models.ImageField(blank=True, null=True)
+    register_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return f'{self.number} - {self.first_name} {self.last_name}'
 
-class Teacher(models.Model):
-    pass
+    class Meta:
+        ordering = ("number",)
+        verbose_name = "Öğrenci"
+        verbose_name_plural = "Öğrenciler"
+        # db_table="" tablo ismi değişmek için
